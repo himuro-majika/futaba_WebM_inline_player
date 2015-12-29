549ef6c980c832e7c6a76ca276fe6a25863e6f67
himuro-majika <himuro-majika@users.noreply.github.com>
2015-12-29 20:12:22 +0900

    Fix mutationobserver

--- futaba_WebM_inline_player.user.js
+++ futaba_WebM_inline_player.user.js
@@ -13,73 +13,59 @@
 this.$ = this.jQuery = jQuery.noConflict(true);
 
 (function ($) {
-
-	add_class_and_rel();
-	// スレ内の画像にクラス、rel属性を付加する
-	function add_class_and_rel() {
-		var AKAHUKU = false, FUTAKURO = false, FUTABOARD = false;
+	init();
+	function init() {
+		var AKAHUKU = false, FUTAKURO = false;
 		// 赤福が有効か
 		if ($("#akahuku_thumbnail").length) { AKAHUKU = true; }
 		// ふたクロが有効か
 		if ($("#master").length) { FUTAKURO = true; }
-		// futaboardか
-		if ($("#threadsbox").length) { FUTABOARD = true; }
-		add_class_and_rel_Thread();
-		add_class_and_rel_Res();
+		getImgNodeThread();
+		getImgNodeRes();
 		if (AKAHUKU || FUTAKURO) {
 			observeInserted();
 		}
 		// スレ画
-		function add_class_and_rel_Thread() {
+		function getImgNodeThread() {
 			var $sure_a = $("body > form > a > img");
 			if (FUTAKURO) { // ふたクロ
 				$sure_a = $("#master > a > img");
 			}
-			if (FUTABOARD) { // futaboard
-				$sure_a = $(".d7 > a > img");
-			}
 			$sure_a.each(function() {
-				addAttr($(this));
+				replaceNode($(this));
 			});
 		}
 		// レス画像
-		function add_class_and_rel_Res() {
-			//  var Start = new Date().getTime();//count parsing time
+		function getImgNodeRes() {
 			var $res_a = $(".rtd > a > img");
-			if (FUTABOARD) { // futaboard
-				$res_a = $(".d6 > table img");
-			}
 			$res_a.each(function() {
-				addAttr($(this));
+				replaceNode($(this));
 			});
-			//  console.log('Parsing : '+((new Date()).getTime()-Start) +'msec');//log parsing time
 		}
 		// 続きを読むで挿入される要素を監視
 		function observeInserted() {
 			var target = $("html > body > form[action]:not([enctype])").get(0);
-			if (FUTABOARD) {
-				target = $(".d6").get(0); // futaboard
-			}
 			var observer = new MutationObserver(function(mutations) {
 				mutations.forEach(function(mutation) {
 					var $nodes = $(mutation.addedNodes);
-					add_class_res_inserted($nodes);
+					replaceNodeInserted($nodes);
 				});
 			});
-			observer.observe(target, { childList: true });			// TODO:動いてない
+			observer.observe(target, { childList: true });
 		}
 		// 挿入されたレスに属性を付加
-		function add_class_res_inserted($nodes) {
-			//  var Start = new Date().getTime();//count parsing time
-			var $res_a_inserted = $nodes.find("td > a > img");
-			addAttr($res_a_inserted);
-			//  console.log('Parsing : '+((new Date()).getTime()-Start) +'msec');//log parsing time
+		function replaceNodeInserted($nodes) {
+			var $res_inserted = $nodes.find("td > a > img");
+			if ($res_inserted.length) {
+				replaceNode($res_inserted);
+			}
 		}
-		// ノードにクラス、属性を付加
-		function addAttr(node) {
+		// ノードの書き換え
+		function replaceNode(node) {
 			// console.log(node);
 			var href = node.parent().attr("href");
 			if (!href.match(/\.webm$/)) {
+				// 拡張子.webm以外
 				return;
 			}
 			var width = node.attr("width");
@@ -91,9 +77,9 @@ this.$ = this.jQuery = jQuery.noConflict(true);
 				css: {
 					"width": width,
 					"height": height,
-					"text-align": "left",
 					"margin": "0 20px",
 					"float": "left",
+					"clear": "left",
 				},
 			}).append(
 				$("<source>", {
