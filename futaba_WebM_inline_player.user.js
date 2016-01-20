dba4914036851f41996b41b2ce4575f8c5b4ab5a
himuro-majika <himuro-majika@users.noreply.github.com>
2016-01-20 19:55:13 +0900

    Add exclude pages

--- futaba_WebM_inline_player.user.js
+++ futaba_WebM_inline_player.user.js
@@ -5,6 +5,7 @@
 // @author      himuro_majika
 // @include     http://*.2chan.net/*/*
 // @exclude     http://*.2chan.net/*/futaba.php?mode=cat*
+// @exclude     http://*.2chan.net/bin/*
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @require     https://greasyfork.org/scripts/1884-gm-config/code/GM_config.js?version=4836
 // @version     1.6.1
